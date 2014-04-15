af950922107e3a0781201ca126e8d1c4711181b2
GregOwen <greowen@gmail.com>
2014-04-14 20:36:11 -0400

    Many more complicated things

--- regression.py
+++ regression.py
@@ -1,8 +1,9 @@
 import csv
 import sys
 import numpy as np
-from sklearn import linear_model
 import matplotlib.pyplot as plt
+
+from sklearn import linear_model, preprocessing, cross_validation
 
 dataset = sys.argv[1] if len(sys.argv) > 1 else "Bike-Sharing-Dataset/day.csv"
 
@@ -12,7 +13,8 @@ Y = []
 
 columns = []
 
-X_columns = ["workingday", "atemp","hum","windspeed"] # TODO(Bieber): Turn weekday into multiple indicator variables
+X_columns = ["yr"]#, "workingday", "hum","windspeed", "atemp"]
+prettyNames = ["year"]#, "workingday", "humidity", "windspeed", "temperature"]
 Y_columns = ["cnt"]
 
 X_indices = []
@@ -38,6 +40,8 @@ with open(dataset, 'rb') as csvfile:
                     data = [0] * 6
                     if day != 6:
                         data[day] = 1
+                elif columns[j] == "atemp":
+                    data = [float(data), float(data)**2, float(data)**3]
                 elif j in X_indices or j in Y_indices:
                     data = [float(data)]
 
@@ -49,9 +53,73 @@ with open(dataset, 'rb') as csvfile:
             X.append(x)
             Y.append(y)
 
-clf = linear_model.LinearRegression()
+# clf = linear_model.LinearRegression()
 # clf = linear_model.Ridge(alpha = .5)
-clfLasso = linear_model.Lasso(alpha = .2)
+# clfLasso = linear_model.Lasso(alpha = .2)
+
+X = np.array(X)
+Y = np.array(Y)[:,0]
+
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, random_state=43)
+
+clf = linear_model.LinearRegression()
+fit = clf.fit(X_train, Y_train)
+print "unconstrained:"
+print fit.coef_
+print ""
+
+kf = cross_validation.KFold(len(Y_train), n_folds=10, shuffle=True)
+clfLassoCV = linear_model.LassoCV(cv=kf)
+lassoCV = clfLassoCV.fit(X_train, Y_train)
+print "lasso:"
+print lassoCV.alpha_
+print lassoCV.coef_
+print ""
+
+clfRidgeCV = linear_model.RidgeCV(cv=kf)
+ridgeCV = clfRidgeCV.fit(X_train, Y_train)
+print "ridge:"
+print ridgeCV.alpha_
+print ridgeCV.coef_
+print ""
+
+print "Scores (train):"
+print "unconstrained: ", fit.score(X_train, Y_train)
+print "lasso: ", lassoCV.score(X_train, Y_train)
+print "ridge: ", ridgeCV.score(X_train, Y_train)
+print ""
+
+print "Scores (test):"
+print "unconstrained: ", fit.score(X_test, Y_test)
+print "lasso: ", lassoCV.score(X_test, Y_test)
+print "ridge: ", ridgeCV.score(X_test, Y_test)
+
+def plotLassoCoeffs():
+    numVars = len(X_train[0])
+    maxLen = 400
+    coeffs = np.array([linear_model.Lasso(alpha=a).fit(X_train,Y_train).coef_ for a in range(1,maxLen + 1)])
+    filteredCoeffs = [coeffs[abs(coeffs[:,i]) > 0,i] for i in range(numVars)]
+
+    plots = [None] * numVars
+
+    for i in range(numVars):
+        if filteredCoeffs[i].shape[0] < maxLen:
+            plots[i], = plt.semilogx(np.append(filteredCoeffs[i], 0))
+        else:
+            plots[i], = plt.semilogx(filteredCoeffs[i])
+
+    plt.axvline(x=ridgeCV.alpha_, color='k')
+    #plt.axis([1,1000,-4000,8000])
+    plt.gca().set_xlim([1,1000])
+
+    plt.legend(plots, prettyNames)
+    plt.xlabel("alpha (l1 norm parameter)")
+    plt.ylabel("weight")
+
+plotLassoCoeffs()
+
+"""
+normX = preprocessing.normalize(X, norm='l1', axis=0)
 
 print clf.fit(X, Y)
 print clf.coef_
@@ -60,11 +128,19 @@ print clf.intercept_
 print clfLasso.fit(X, Y)
 print clfLasso.coef_
 
-colors = ['r', 'g', 'b']
+plt.figure(1)
+plt.scatter([x[3] for x in normX], [y[0] for y in Y], c=[x[0] for x in normX])
+plt.figure(2)
+plt.scatter([x[3] for x in X], [y[0] for y in Y], c=[x[0] for x in X])
+plt.figure(3)
+plt.scatter([x[3] for x in X], [x[-2]/float(x[-1]) for x in X], c=[x[0] for x in X])
+
+colors = ['r', 'g', 'b', 'y']
 
 for i in range(len(X_columns)):
     plt.figure(i)
     plt.scatter([x[i] for x in X], [y[0] for y in Y], c=colors[i])
-    plt.plot([x[i] for x in X], [clf.coef_[0][i]*x[i] for x in X], color='b')
-
+    plt.plot([x[i] for x in X], clf.intercept_ + [clf.coef_[0][i]*x[i] for x in X], color='b')
+"""
 plt.show()
+
